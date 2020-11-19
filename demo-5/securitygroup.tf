3b4cdf12ccb61c3e927d00b3d0bd5d753752b84c
hesamhamdarsi <32292326+hesamhamdarsi@users.noreply.github.com>
2020-11-19 19:25:17 +0330

    Update securitygroup.tf

--- demo-5/securitygroup.tf
+++ demo-5/securitygroup.tf
@@ -3,6 +3,7 @@ data "aws_ip_ranges" "european_ec2" {
   services = ["ec2"]
 }
 
+#after creating this rule, you will be able to see a security group called "from_europe" in your AWS security groups with the added rules
 resource "aws_security_group" "from_europe" {
   name = "from_europe"
 
@@ -10,6 +11,10 @@ resource "aws_security_group" "from_europe" {
     from_port   = "443"
     to_port     = "443"
     protocol    = "tcp"
+    #data refer to the data
+    #aws_ip_ranges refer to the type of source
+    #european_ec2 refer to the name
+    #cidr_blocks refer to the attribute
     cidr_blocks = slice(data.aws_ip_ranges.european_ec2.cidr_blocks, 0, 50)
   }
   tags = {
