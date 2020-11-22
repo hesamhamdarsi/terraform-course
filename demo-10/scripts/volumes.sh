71b57b8633ee5cd0c139b6b50a6041c226c6bf0d
hesamhamdarsi <32292326+hesamhamdarsi@users.noreply.github.com>
2020-11-22 14:05:30 +0330

    Update volumes.sh

--- demo-10/scripts/volumes.sh
+++ demo-10/scripts/volumes.sh
@@ -2,8 +2,13 @@
 
 set -ex 
 
+#it referesh the LVM state to see if the LVM volumes already present
 vgchange -ay
 
+# then we're going to look for DEVICE variable which is "xvdh" volume we used to attach in instance file
+# thie below command will output the file system of device
+# if it doesn't have a file system, then it will create a physical volume, logical volum named "volume1" and then format it
+# if it has, then it go to the line 31 -> "mkdir -p /data"
 DEVICE_FS=`blkid -o value -s TYPE ${DEVICE} || echo ""`
 if [ "`echo -n $DEVICE_FS`" == "" ] ; then 
   # wait for the device to be attached
@@ -21,6 +26,8 @@ if [ "`echo -n $DEVICE_FS`" == "" ] ; then
   lvcreate --name volume1 -l 100%FREE data
   mkfs.ext4 /dev/data/volume1
 fi
+# then we will make a directory to mount (if it's not created yet. -p will check it)
+# and then we'll add it to fstab if its not added yet and then we mount it
 mkdir -p /data
 echo '/dev/data/volume1 /data ext4 defaults 0 0' >> /etc/fstab
 mount /data
