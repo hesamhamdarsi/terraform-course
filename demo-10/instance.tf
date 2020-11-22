a9ca05f8c346aa2dac7f40dc136f0b8ce4e034cf
hesamhamdarsi <32292326+hesamhamdarsi@users.noreply.github.com>
2020-11-22 13:45:48 +0330

    Update instance.tf

--- demo-10/instance.tf
+++ demo-10/instance.tf
@@ -1,3 +1,4 @@
+#our goal is to create an EBS volume, attach it, format it, and mount it 
 resource "aws_instance" "example" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
@@ -12,9 +13,11 @@ resource "aws_instance" "example" {
   key_name = aws_key_pair.mykeypair.key_name
 
   # user data
+  # here We have our rendered template_cloudinit_config 
   user_data = data.template_cloudinit_config.cloudinit-example.rendered
 }
 
+# we create a volume (in the first time) and for the second time of execution, it checks the state file and if volume is already created it wont create it
 resource "aws_ebs_volume" "ebs-volume-1" {
   availability_zone = "eu-west-1a"
   size              = 20
@@ -24,6 +27,7 @@ resource "aws_ebs_volume" "ebs-volume-1" {
   }
 }
 
+# we attatch this created volume to the instance
 resource "aws_volume_attachment" "ebs-volume-1-attachment" {
   device_name  = var.INSTANCE_DEVICE_NAME
   volume_id    = aws_ebs_volume.ebs-volume-1.id
